5ea9542dcdc68977da778ba408d1dab5d09f70cd
Peter D Titan <peterdtitan@Peters-MacBook-Pro.local>
2022-06-17 08:11:54 +0300

    Input validation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Mobile Menu Starts Here
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const logo = document.querySelector('.logo');
@@ -15,3 +16,18 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
   navMenu.classList.remove('active');
   logo.classList.remove('active');
 }));
+
+// Email Validation
+document.querySelector('.submit').addEventListener('click', (event) => {
+  const email = document.getElementById('email').value;
+  const error = document.querySelector('.error');
+  const isLowerCase = (str) => str === str.toLowerCase();
+  if (!isLowerCase(email)) {
+    event.preventDefault();
+    error.innerHTML = 'Error: email should be in lowercase!';
+    // hide error 5 seconds time so it can show up if the there is another error when click again
+    setTimeout(() => {
+      error.innerHTML = '';
+    }, 5000);
+  }
+});
